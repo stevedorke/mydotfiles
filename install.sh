1620d3e412ab86b601e627d0c48498fb1de22dc8
Steven Lee <steven.lee@defymedia.com>
2016-06-29 14:37:03 +0800

    fixing install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
-# Get current dir (so run this script from anywhere)
-export DOTFILES_DIR EXTRA_DIR
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-EXTRA_DIR="$HOME/.extra"
-
-ln -sfv “$DOTFILES_DIR/.mydotfiles/.bash_profile” ~
-ln -sfv “$DOTFILES_DIR/.mydotfiles/.git-completion.sh” ~
-ln -sfv “$DOTFILES_DIR/.mydotfiles/.git-prompt.sh” ~
-ln -sfv “$DOTFILES_DIR/.mydotfiles/.gitconfig” ~
-ln -sfv “$DOTFILES_DIR/.mydotfiles/.gitexcludes” ~
-ln -sfv “$DOTFILES_DIR/.mydotfiles/.gitignore_global” ~
-ln -sfv “$DOTFILES_DIR/.mydotfiles/.inputrc” ~
+ln -sfv “~/.mydotfiles/.bash_profile” ~
+ln -sfv “~/.mydotfiles/.git-completion.sh” ~
+ln -sfv “~/.mydotfiles/.git-prompt.sh” ~
+ln -sfv “~/.mydotfiles/.gitconfig” ~
+ln -sfv “~/.mydotfiles/.gitexcludes” ~
+ln -sfv “~/.mydotfiles/.gitignore_global” ~
+ln -sfv “~/.mydotfiles/.inputrc” ~
