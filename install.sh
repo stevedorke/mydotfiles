08f01835cb312042b65434bb855db56c8924bc86
Steven Lee <info@steven-lee.com>
2017-05-06 18:32:20 +0800

    migrating bash to oh-my-zsh

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -sfv $DOTFILES_DIR/.bash_profile ~
-ln -sfv $DOTFILES_DIR/.bashrc ~
-ln -sfv $DOTFILES_DIR/.git-completion.bash ~
-ln -sfv $DOTFILES_DIR/.git-prompt.sh ~
+ln -sfv $DOTFILES_DIR/.zshrc ~
 ln -sfv $DOTFILES_DIR/.gitconfig ~
 ln -sfv $DOTFILES_DIR/.gitexcludes ~
 ln -sfv $DOTFILES_DIR/.gitignore_global ~
-ln -sfv $DOTFILES_DIR/.inputrc ~
