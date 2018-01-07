09127d603da59f7f563b37d930a5a4dbc54e79c4
Steven Lee <info@steven-lee.com>
2018-01-07 12:11:23 -0800

    re-adding dock show/hide animation

--- install.sh
+++ install.sh
@@ -260,7 +260,7 @@ defaults write com.apple.dock mru-spaces -bool false
 # Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
 # Remove the animation when hiding/showing the Dock
-defaults write com.apple.dock autohide-time-modifier -float 0
+# defaults write com.apple.dock autohide-time-modifier -float 0
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
