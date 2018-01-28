d7c1693ec9e8f03a9ebaec29188f7a877312d03f
Steven Lee <info@steven-lee.com>
2018-01-28 11:26:33 -0800

    update cask packages

--- install.sh
+++ install.sh
@@ -21,11 +21,11 @@ brew install ack ccat coreutils git node thefuck tldr trash tree wget yarn zsh z
 
 #brew cask installs (includes https://github.com/sindresorhus/quick-look-plugins)
 brew tap caskroom/cask
-brew cask install adapter alfred android-file-transfer appcleaner caffeine dropbox flux gas-mask google-chrome google-chrome-canary google-drive iterm2 karabiner-elements postman slack spotify spotify-notifications the-unarchiver transmission virtualbox visual-studio-code vlc qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install adapter alfred android-file-transfer appcleaner caffeine dropbox flux gas-mask google-chrome google-chrome-canary insomnia iterm2 karabiner-elements keycastr postman slack spotify spotify-notifications the-unarchiver transmission visual-studio-code vlc qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook quicklookase qlvideo
 
 #brew font install
 brew tap caskroom/fonts
-brew cask install font-source-code-pro font-source-code-pro-for-powerline
+brew cask install font-source-code-pro-for-powerline
 
 #install oh my zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
